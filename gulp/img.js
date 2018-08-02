2e50900d8a3294786393c895e6f6aecd10b6e3e4
andrey4ikk <33235126+andrey4ikk@users.noreply.github.com>
2018-08-02 19:02:29 +0300

    Update img.js

--- gulp/img.js
+++ gulp/img.js
@@ -7,7 +7,9 @@ var imagemin                = require('gulp-imagemin');
 
 
 gulp.task('img', function () {
-    gulp.src(cnf.src.img.noCompress) 
+    gulp.src(cnf.src.img.noCompress)
+        .pipe(gulp.dest(cnf.dist.img));
+    gulp.src(cnf.src.img.compress)
         .pipe(imagemin([
             imagemin.gifsicle({interlaced: true}),
             imagemin.jpegtran({progressive: true}),
@@ -20,10 +22,8 @@ gulp.task('img', function () {
             })
         ]))
         .pipe(gulp.dest(cnf.dist.img));
-    gulp.src(cnf.src.img.all) 
-        .pipe(gulp.dest(cnf.dist.img))
   });
    
   gulp.task('img:watch', function () {
-    gulp.watch([cnf.src.js, 'src/js/components/**/*.*'], ['img']);
+    gulp.watch(['src/img/**/*.*'], ['img']);
   });
